6acc942070e4c5aacf4e01a8ceaefcd4ffb8b046
Lawunn <lawunnkhaing.tgi@gmail.com>
2024-02-02 22:58:47 +0200

    Test

--- server.js
+++ server.js
@@ -1,52 +1,56 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const bodyParser = require('body-parser');
 
-const app = express();
+// const app = express();
 
-// Ignore favicon requests
-app.get('/favicon.ico', (req, res) => res.status(204).end());
+// // Handle favicon requests
+// app.get('/favicon.ico', (req, res) => res.status(204).end());
 
-// Connect to MongoDB Atlas
-mongoose.connect("mongodb+srv://Lawunn:Petra1005@cluster.1qhaeok.mongodb.net/Contact", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+// // Connect to MongoDB Atlas
+// mongoose.connect("mongodb+srv://Lawunn:Petra1005@cluster.1qhaeok.mongodb.net/Contact", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// });
 
-const dataSchema = new mongoose.Schema({
-  fullName: String,
-  email: String,
-  message: String
-});
+// const dataSchema = new mongoose.Schema({
+//   fullName: String,
+//   email: String,
+//   message: String
+// });
 
-const Contact = mongoose.model('Data', dataSchema);
+// const Contact = mongoose.model('Data', dataSchema);
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/contact.html');
-});
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/contact.html');
+// });
 
-app.post('/contact', async (req, res) => {
-  const newData = new Contact({
-    fullName: req.body.fullName,
-    email: req.body.email,
-    message: req.body.message
-  });
+// app.post('/contact', async (req, res) => {
+//   const newData = new Contact({
+//     fullName: req.body.fullName,
+//     email: req.body.email,
+//     message: req.body.message
+//   });
 
-  try {
-    await newData.save();
-    // Redirect back to contact.html with a success query parameter
-    res.redirect('/contact.html?status=success');
-  } catch (err) {
-    console.error(err);
-    // Redirect back to contact.html with an error query parameter
-    res.redirect('/contact.html?status=error');
-  }
-});
+//   try {
+//     await newData.save();
+//     // Redirect back to contact.html with a success query parameter
+//     res.redirect('/contact.html?status=success');
+//   } catch (err) {
+//     console.error(err);
+//     // Redirect back to contact.html with an error query parameter
+//     res.redirect('/contact.html?status=error');
+//   }
+// });
 
-// Serve the contact.html file for any other route
+// // Serve the contact.html file for any other route
+// app.get('*', (req, res) => {
+//   res.sendFile(__dirname + '/contact.html');
+// });
+
+// module.exports = app;
 
 
-module.exports = app;
